617a8bc5404e635105fbcd7f3a11144b71fa31fa
guganems <g_nemsveridze@cu.edu.ge>
2020-08-03 13:40:11 +0400

    Dasd

---
+++ smartpag.js
@@ -0,0 +1,14 @@
+class SmartPag {
+    constructor(items, config) {
+        this.config = config;
+        this.init();
+    }
+    init () {
+        // TODO: initialization of the first page
+        // TODO: add buttons
+        this.drawTable();
+    }
+    drawTable(){
+        // TODO: draws it
+    }
+}
