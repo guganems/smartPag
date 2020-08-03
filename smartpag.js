2d2dffe9623dbe646a387df27279baef8a0f98c7
GioCharkviani <giorgi.charkviani00@gmail.com>
2020-08-03 19:37:03 +0400

    Dasd

--- smartpag.js
+++ smartpag.js
@@ -1,14 +1,88 @@
 class SmartPag {
     constructor(items, config) {
         this.config = config;
+        this.items = items;
+        this.pagesBtns =  Math.ceil(this.items.length/this.config.quantity);
+        this.starter = 0;
         this.init();
     }
     init () {
         // TODO: initialization of the first page
+        this.container = document.getElementById('container');
+
+
         // TODO: add buttons
         this.drawTable();
+
+        let newBtns = ``;
+        for(let i = 1; i <= this.pagesBtns; i++) {
+            newBtns += `
+                <button style="margin: 0 5px" class="${i}">${i}</button>
+            `;
+        }
+
+        this.container.innerHTML += `
+            <div style="display: flex; padding-top: 5px">
+                <button class="pre">pre</button>
+                    <div class="btns">
+                        ${newBtns}
+                    </div>
+                <button class="next">next</button>
+            </div>
+        `;
+
+        this.chnageItems();
     }
     drawTable(){
         // TODO: draws it
+        let newDivs = ``;
+        for(let i = this.starter; i < this.starter+this.config.quantity; i++) {
+            if(i < this.items.length) {
+                newDivs += `
+                <div style="display: flex; margin: 5px 0">
+                    <p style="margin: 0 5px">${this.items[i].name}</p>
+                    <p style="margin: 0 5px">${this.items[i].age}</p>
+                </div>
+            `;
+            }
+        }
+        
+        this.container.innerHTML = `
+            <div style="">
+                <div style="display: flex">
+                    <h3 style="margin: 5px">${this.config.headers[0]}</h3>
+                    <h3 style="margin: 5px">${this.config.headers[1]}</h3>
+                </div>
+                ${newDivs}
+            </div>
+        `;
     }
+
+    chnageItems() {
+        // TODO: set new items
+
+        document.querySelectorAll('button').forEach(e => {
+            let me = this;
+            e.addEventListener('click', function() {
+                if(e.classList.contains('pre')) {
+                    if(me.starter > 0) {
+                        me.starter -= me.config.quantity;
+                        me.init();
+                    }
+                }
+                else if(e.classList.contains('next')) {
+                    if(me.starter < me.items.length-me.config.quantity) {
+                        me.starter += me.config.quantity;
+                        me.init();
+                    }
+                }
+                else if(e.parentElement.classList.contains('btns')) {
+                    me.starter = Number(e.textContent)*me.config.quantity - me.config.quantity;
+                    me.init();
+                }
+            });
+        });
+    }
+
 }
+
